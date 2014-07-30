eb06e1e41a4ec28b40fe76b7db6723511317ca5d
Pavel Abrosimov <p.abrosimou@gmail.com>
2014-07-31 02:24:57 +0300

    was edited list of articles

--- app/scripts/services/articleServices.js
+++ app/scripts/services/articleServices.js
@@ -14,7 +14,9 @@ angular.module('angularProjectApp')
                     console.log("Error");
                 })
         }
-     
+
+
+
         return{
             articles: _articles,
             getArticles: _getArticles
